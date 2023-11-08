b4ab393ceb502903906ddb6a9c468f855eff5983
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2023-11-08 15:33:20 +0900

    Deviseの導入とユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :family_name,
+    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+    presence: true
+  validates :first_name,
+    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+    presence: true
+  validates :family_name_kana,
+    format: { with: /\A([ァ-ン]|ー)+\z/},
+    presence: true
+  validates :first_name_kana,
+    format: { with: /\A([ァ-ン]|ー)+\z/},
+    presence: true
+  validates :birth, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+end

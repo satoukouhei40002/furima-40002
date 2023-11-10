d6aaf085ea1e89de5c4e2cbfdcd8164098e24c32
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2023-11-10 23:12:42 +0900

    商品出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,31 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to_active_hash :shipping_cost, class_name: 'ShippingCost'
+  belongs_to :prefecture
+  belongs_to :number_of_days
+
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :introduction
+    validates :category_id,
+    numericality: { other_than: 1 , message: "can't be blank"}
+    validates :status_id,
+    numericality: { other_than: 1 , message: "can't be blank"}
+    validates :shipping_cost_id,
+    numericality: { other_than: 1 , message: "can't be blank"}
+    validates :prefecture_id,
+    numericality: { other_than: 1 , message: "can't be blank"}
+    validates :number_of_days_id,
+    numericality: { other_than: 1 , message: "can't be blank"}
+    validates :price,
+    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
+  validates_format_of :price, with: /\A[0-9]+\z/, message: "should contain only half-width numerals"
 end
+    

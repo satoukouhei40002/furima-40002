93816e0ef2fd62059874f1b3c6da17c2460aee6f
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2023-11-13 18:32:43 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
   private
 
   def item_params

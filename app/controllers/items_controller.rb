fb12657ec02474c9d374568546173a3fa05b96ec
satoukouhei40002 <a.k.o.o.z.vv@icloud.com>
2023-11-13 19:32:16 +0900

    商品削除機能の実装の修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
   def index
@@ -34,12 +34,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if user_signed_in? && current_user.id == @item.user_id
       @item.destroy
       redirect_to root_path
-    else
-      redirect_to root_path
-    end
   end
 
   private
